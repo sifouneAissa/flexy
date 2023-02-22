3a2e44c73d950313a2e77eb55088ff8d4292df8a
sifoune aissa <aissa23sifoune@gmail.com>
2023-02-22 09:59:57 +0100

    # levels order

--- app/Http/Livewire/Datatables/LevelTable.php
+++ app/Http/Livewire/Datatables/LevelTable.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Datatables;
 
 use App\Models\Level;
 use App\Models\Provider;
+use Mediconesystems\LivewireDatatables\Action;
 use Mediconesystems\LivewireDatatables\Column;
 use Mediconesystems\LivewireDatatables\DateColumn;
 use Mediconesystems\LivewireDatatables\Http\Livewire\LivewireDatatable;
@@ -28,10 +29,10 @@ class LevelTable extends LivewireDatatable
     {
         //
         return [
-
             Column::checkbox(),
-            NumberColumn::name('id')
-                ->label('ID')->filterable(),
+            NumberColumn::name('order')
+                ->defaultSort('asc')
+                ->label('ORDER')->filterable(),
             Column::name('name')
                 ->searchable(),
             Column::name('description'),
@@ -39,6 +40,24 @@ class LevelTable extends LivewireDatatable
             Column::callback(['id','name'], function ($id,$name) {
                 return view('livewire.table-actions.level-table-actions', ['id' => $id,'item' => $this->builder()->where('id',$id)->first()]);
             })->unsortable()
+        ];
+    }
+
+
+    public function buildActions()
+    {
+        return [
+
+            Action::value('Order')->label('Order')->callback(function ($mode, $items) {
+                // $items contains an array with the primary keys of the selected items
+                if(Level::count() === count($items))
+                foreach($items as $key => $value){
+                    $model = Level::find($value);
+                    $model->order = $key + 1;
+                    $model->save();
+                }
+            }),
+
         ];
     }
 }
