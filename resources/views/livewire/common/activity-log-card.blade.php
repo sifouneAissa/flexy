cfe121c09041df4ca8ef65817d49f82dc31045ba
sifoune aissa <aissa23sifoune@gmail.com>
2023-02-25 15:36:50 +0100

    # fix log card

--- resources/views/livewire/common/activity-log-card.blade.php
+++ resources/views/livewire/common/activity-log-card.blade.php
@@ -1,5 +1,5 @@
 <div>
     @foreach($activities as $activity)
-        <p>{{$all ? $activity->subject?->name : ""}} {{$activity->description}} in {{translateDate($activity->created_at)}} by {{$activity->causer->id === auth()->user()->id ? 'you' : $activity->causer->name}}.</p>
+        <p class="text-secondary">{{$all ? $activity->subject?->name : ""}} {{$activity->description}} in {{translateDate($activity->created_at)}} by {{$activity->causer->id === auth()->user()->id ? 'you' : $activity->causer->name}}.</p>
     @endforeach
 </div>
