f1d4d5d061112d5fd2dac086331df46d0dbde0f7
sifoune aissa <aissa23sifoune@gmail.com>
2023-02-28 19:08:06 +0100

    # add seeders

--- resources/views/livewire/layouts/crud-layout.blade.php
+++ resources/views/livewire/layouts/crud-layout.blade.php
@@ -36,8 +36,6 @@
 
     {{--    select2--}}
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
-
-    <link href="/assets/tailwind/tailwind.min.css" rel="stylesheet">
     @vite(['resources/js/app.js'])
 
     @livewireStyles
